1026e17da8497b7b8eb35f8d30916b06880488cb
HASEE-MATT\Matt <670159357@qq.com>
2017-01-21 22:27:16 +0800

    leetcode the second update

--- student/zhulei/task5/leetcode/keyFormatting.js
+++ student/zhulei/task5/leetcode/keyFormatting.js
@@ -3,35 +3,58 @@
  * @param {number} K
  * @return {string}
  */
+
+ //version 1
+// var licenseKeyFormatting = function(S, K) {
+//     var reg1 = /-/g;
+//     var str = S.replace(reg1, '').toUpperCase();
+//     var firstDash = str.length % K;
+//     var key = '';
+
+//     if (firstDash === 0) {
+//     	var tmp = '';
+//     	for (var i = 0; i < str.length; i++) {
+//     		tmp += str[i];
+//     		if (tmp.length === K) {
+//     			key += tmp + '-';
+//     			tmp = '';
+//     		}
+//     	}
+//     	key = key.slice(0, -1);
+//     } else {
+//     	key = str.slice(0, firstDash) + '-';
+//     	var tmp = '';
+//     	for (var i = firstDash; i < str.length; i++) {
+//     		tmp += str[i];
+//     		if (tmp.length === K) {
+//     			key += tmp + '-';
+//     			tmp = '';
+//     		}
+//     	}
+//     	key = key.slice(0, -1);
+//     }
+
+//     console.log(key);
+//     return key;
+// };
+
+//version 2
 var licenseKeyFormatting = function(S, K) {
     var reg1 = /-/g;
     var str = S.replace(reg1, '').toUpperCase();
     var firstDash = str.length % K;
+    var reg2 = new RegExp('(.{'+K+'})', 'g');
     var key = '';
 
     if (firstDash === 0) {
-    	var tmp = '';
-    	for (var i = 0; i < str.length; i++) {
-    		tmp += str[i];
-    		if (tmp.length === K) {
-    			key += tmp + '-';
-    			tmp = '';
-    		}
-    	}
-    	key = key.slice(0, -1);
+        key = str.replace(reg2, '$1-');
+        key = key.slice(0, -1);        
     } else {
-    	key = str.slice(0, firstDash) + '-';
-    	var tmp = '';
-    	for (var i = firstDash; i < str.length; i++) {
-    		tmp += str[i];
-    		if (tmp.length === K) {
-    			key += tmp + '-';
-    			tmp = '';
-    		}
-    	}
-    	key = key.slice(0, -1);
+        key = str.slice(0, firstDash) + '-';
+        str = str.slice(firstDash, str.length);
+        key += str.replace(reg2, '$1-');
+        key = key.slice(0, -1);
     }
-
     console.log(key);
     return key;
 };
--- student/zhulei/task5/leetcode/keyFormatting.md
+++ student/zhulei/task5/leetcode/keyFormatting.md
@@ -21,9 +21,10 @@ Output: "24-A0R-74K"
 值得注意的是，当S的长度不能被K整除时，输出结果的第一部分作为调整长度可变，其余部分按照K的长度进行划分。
 
 ## 解法一
-最容易想到的就是循环处理，不过用循环来处理一个字符串似乎看起来很笨。 但在我查询了一遍js原生的字符串处理函数和正则表达式之后，发现似乎没有方法可以直接快速的按照这题的要求处理，所以又回到循环的路子上来了。   
+最容易想到的就是循环处理。   
 
-在对初始字符串S进行去`'-'`和大写化处理之后，需要对字符串S能否被K整除进行分类处理。    
+在对初始字符串S进行去`'-'`和大写化处理之后，需要对字符串S能否被K整除进行分类处理。值得注意的是，字符串在加上`'-'`之后，长度会发生变化，所以我增加了一个中间变量tmp来和K
+进行比较来简化思路。   
 得到了时间复杂度为O(n)的解法一：
 <pre>
 var licenseKeyFormatting = function(S, K) {
@@ -59,5 +60,28 @@ var licenseKeyFormatting = function(S, K) {
     return key;
 };
 </pre>
-值得注意的是，字符串在加上`'-'`之后，长度会发生变化，所以我增加了一个中间变量tmp来和K
-进行比较来简化思路。
+
+## 解法二
+在好好恶补了一阵正则表达式的知识之后，我又找的了一种用正则的解法：
+<pre>
+var licenseKeyFormatting = function(S, K) {
+    var reg1 = /-/g;
+    var str = S.replace(reg1, '').toUpperCase();
+    var firstDash = str.length % K;
+    var reg2 = new RegExp('(.{'+K+'})', 'g');
+    var key = '';
+
+    if (firstDash === 0) {
+        key = str.replace(reg2, '$1-');
+        key = key.slice(0, -1);        
+    } else {
+        key = str.slice(0, firstDash) + '-';
+        str = str.slice(firstDash, str.length);
+        key += str.replace(reg2, '$1-');
+        key = key.slice(0, -1);
+    }
+    console.log(key);
+    return key;
+};
+</pre>
+代码相对于循环而言简洁了不少，不过运行效率基本和循环持平。
