be9118182ff7c7d11e332f82f404a91cf80cba46
HASEE-MATT\Matt <670159357@qq.com>
2017-03-03 20:03:19 +0800

    gotop update

---
+++ student/zhulei/task6/goTop/gotop.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+  const threshold = 50; //滚动 50px 之后出按钮
+
+  function GoTop(id){
+    this.button = document.getElementById(id);
+    
+    var self = this;
+    
+    this.button.onclick = function(){
+      // window.scrollTo(0, 0);
+      goTopAnimation();
+    }
+    
+    window.onscroll = function(evt){
+      self.update();
+    }
+    
+    this.update();
+  }
+
+  function goTopAnimation(el){
+    let startTime = Date.now(),
+        duration = 500;
+        currentY = window.scrollY;
+    
+    requestAnimationFrame(function update(){
+      let currentTime = Date.now();
+      let p = (currentTime - startTime) / duration;
+      window.scrollTo(0, currentY * (1 - p));
+      if(window.scrollY){
+        requestAnimationFrame(update);
+      }
+    });
+  }
+
+  GoTop.prototype.update = function(){
+    if(window.scrollY > threshold){
+      this.button.className = 'scroll';
+    }else{
+      this.button.className = '';
+    }
+  };
+
+  var gotop = new GoTop('go-top');
+};
