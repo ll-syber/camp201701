4872d0475eb0d3b298a52c32756df724b0ca2e6c
HASEE-MATT\Matt <670159357@qq.com>
2017-03-23 12:19:01 +0800

    update

--- student/zhulei/task6/inturn/slide.js
+++ student/zhulei/task6/inturn/slide.js
@@ -67,36 +67,69 @@ window.onload = function () {
 	/**
 	*控制组件
 	**/
-	function Controler (id, obj) {
+	function Controler (id, selectCls, evtType) {
 		// Slider.call(this, id, cls, selectCls, interval);
 		this.controler = document.getElementById(id);
 		this.ctrlItems = controler.querySelectorAll('li');
-		this.targetObj = obj;
+		// this.targetObj = obj;
+		this.selectCls = selectCls;
+		this.evtType = evtType;
 	}
+	Controler.prototype.getSelectedItem = function() {
+		return this.controler.querySelector('.'+this.selectCls);
+	};
+	Controler.prototype.getSelectedItemIndex = function() {
+		return Array.from(this.ctrlItems).indexOf(this.getSelectedItem());
+	};
+	Controler.prototype.slideTo = function(index){
+		let selector = this.getSelectedItem();
+		let item = this.ctrlItems[index];
+		if (selector) {
+			selector.className = '';
+		}
+		if (item) {
+			item.className = this.selectCls;
+		}
+	};	
 	Controler.prototype.setIndex = function(){
 		for (let i = 0; i < this.ctrlItems.length; i++) {
 			this.ctrlItems[i].setAttribute('index', i);
 		}
 	};
 	Controler.prototype.setTrigger = function(){
-		this.controler.addEventListener('mouseover', evt => {
+		this.controler.addEventListener(this.evtType, evt => {
 			if (evt.target && evt.target.nodeName == 'LI') {
 				console.log(evt.target.getAttribute('index') - 0);
-				this.targetObj.slideTo(evt.target.getAttribute('index') - 0);
+				// this.targetObj.slideTo(evt.target.getAttribute('index') - 0);
+				this.slideTo(evt.target.getAttribute('index') - 0);
 			}
 		});
 	};
 
 	//单向绑定
-	function bindComponent(obj1, obj2) {
-		
+	function bindComponent(controler, slider, evtType) {
+		controler.controler.addEventListener(evtType, evt => {
+			if (evt.target && evt.target.nodeName == 'LI') {
+				console.log(evt.target.getAttribute('index') - 0);
+				// this.targetObj.slideTo(evt.target.getAttribute('index') - 0);
+				slider.slideTo(evt.target.getAttribute('index') - 0);
+				controler.slideTo(evt.target.getAttribute('index') - 0);
+			}
+		});
+		setInterval(() => {
+			controler.slideTo(slider.getSelectedItemIndex());
+		}, slider.interval);
+		// controler.slideTo(slider.getSelectedItemIndex());
+		// slider.slideTo(controler.getSelectedItemIndex());
 	}
 
-	let imgSlider = new Slider ('imgList', 'slide-list__item', 'slide-list__item--select', 3000);
+	let imgSlider = new Slider ('imgList', 'slide-list__item', 'slide-list__item--selected', 3000);
 	// imgSlider.setReverse();
 	imgSlider.start();
 	// imgSlider.gItm();
-	let imgCtrl = new Controler('controler', imgSlider);
-	imgCtrl.setIndex();
-	imgCtrl.setTrigger();
+	let imgControler = new Controler('controler', 'controler-item--selected', 'mouseover');
+	imgControler.setIndex();
+	// imgControler.setTrigger();
+
+	bindComponent(imgControler, imgSlider, 'mouseover');
 };
